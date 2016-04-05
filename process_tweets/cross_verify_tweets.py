9f7c2724d959e1687318ea6be623afa983b5a121
ericshape <wangjiprc@gmail.com>
2016-04-05 11:51:58 -0400

    init commit and add two classes for tweets retrieve and tweets cross validation.

---
+++ process_tweets/cross_verify_tweets.py
@@ -0,0 +1,8 @@
+# created by Ji Wang ericshape @ 4/5/16 11:47 AM
+
+class Cross_Verify_Tweets():
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    cross_verify_tweets = Cross_Verify_Tweets()
---
+++ process_tweets/fetch_tweet_by_id.py
@@ -0,0 +1,10 @@
+# created by Ji Wang ericshape @ 4/5/16 11:48 AM
+
+class Fetch_Tweet_By_ID():
+
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+
+    fetch_tweet_by_id = Fetch_Tweet_By_ID()
